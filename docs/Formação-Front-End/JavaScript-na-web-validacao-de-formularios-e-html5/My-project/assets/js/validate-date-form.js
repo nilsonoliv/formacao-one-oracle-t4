08af8699371580a8ca875e5ee07f22026ab93383
nilsonoliv <nilsonespiritosant@gmail.com>
2023-03-10 19:13:34 -0300

    personalizando as mensagens de erro pelo js

--- docs/Formação-Front-End/JavaScript-na-web-validacao-de-formularios-e-html5/My-project/assets/js/validate-date-form.js
+++ docs/Formação-Front-End/JavaScript-na-web-validacao-de-formularios-e-html5/My-project/assets/js/validate-date-form.js
@@ -7,21 +7,77 @@ inputs.forEach(input =>
             validaInput(evento.target);
         })
     })
-
-function validaInput(input) // funçao vai validar todos os inputs   
-{
+    
+    function validaInput(input) // funçao vai validar todos os inputs   
+{    
     const typeInput = input.dataset.tipo;    
     if (validators[typeInput]) 
     {
-        validators[typeInput](input)    
+        validators[typeInput](input);    
+    }
+    
+    if (input.validity.valid)
+    {
+        input.parentElement.classList.remove('input-container--invalido');  
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';  
+    } else
+    {
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = pegaMensagemDeErro(inputType, input);  
+    }
+
+    const errorType = 
+    [
+        'valueMissing',
+        'typeMismatch',
+        'patternMismatch',
+        'customError'
+    ]
+
+    const errorMessage = 
+    {
+        name:
+        {
+            valueMissing: 'O campo dnamenão pode estar vazio.'
+        },
+        email:
+        {
+            valueMissing: 'O campo  de email não pode estar vazio.',
+            typeMismatch: 'O email digitado não é valido.'
+        },
+        password:
+        {
+            valueMissing: 'O campo de senha não pode estar vazio.',
+            patternMismatch: 'A senha deverá ter entre 6 e 12 dígitos, deverá ter ao menos uma letra maiúscula, uma minúscula, um número e não poderá ter nenhum caracter especial.'
+        },
+        dateBirth:
+        {
+            valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+            customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+        },
     }
 }
 
 const validators = 
-{
+{   
     dateBirth:input => validateDateBirth(input)
 }
 
+function pegaMensagemDeErro(inputType, input)
+{
+    let mesenger = '';
+
+    errorType.forEach(erro => 
+        {
+            if(input.validity[erro])
+            {
+                mensagem = errorMessage[inputType][erro];
+            }
+        })
+    return mesenger;
+}
+
+//-----------------------------------------------------------------------------------
 function validateDateBirth(input) //faz a validação do input data, se o usuario é ou não maior que 18 anos
 {
     const receivedDate = new Date(input.value);
@@ -41,13 +97,4 @@ function biggerThen18(date) //Faz a comparação entre a data atual com a data i
     return dateBiggerThen18 <= dateNow;
 }
 
-function validateName(input)
-{
-    if (input.validity.valid)
-    {
-        input.parentElement.classList.remove('input-container--invalido');    
-    } else
-    {
-        input.parentElement.classList.add('input-container--invalido');
-    }
-}
+
